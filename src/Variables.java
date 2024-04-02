3195b1042575d5f10f7065424b306f3e9d7ee348
iamrishab2701 <singhrishab166@gmail.com>
2024-04-03 00:04:19 +0530

    Initial commit

---
+++ src/Variables.java
@@ -0,0 +1,14 @@
+public class Variables {
+    public static void main(String[] args)
+    {
+        //Initializing the variable //Declaration Statement
+        int myFirstNumber = 5; //Int is the data type and the = is assignment operator
+        System.out.println(myFirstNumber);
+        myFirstNumber = 10;
+        System.out.println(myFirstNumber);
+
+        //Sum of two numbers
+        myFirstNumber = 10+5;
+        System.out.println(myFirstNumber);
+    }
+}
