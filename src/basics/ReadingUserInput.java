a450f0f7c28dfe2ba385d3893ce55aa7b8165899
iamrishab2701 <singhrishab166@gmail.com>
2024-04-17 21:57:58 +0530

    New Changes

---
+++ src/basics/ReadingUserInput.java
@@ -0,0 +1,26 @@
+package basics;
+
+public class ReadingUserInput {
+
+    public static void main(String[] args) {
+
+        int currentYear = 2024;
+        System.out.println(getInputFromConsole(currentYear));
+        // System.out.println(getInputFromScanner(currentYear));
+    }
+
+    public static String getInputFromConsole(int currentYear) {
+
+        String name = System.console().readLine("Hi, What's your name? "); // To print using System.console use the
+                                                                           // terminal and enter command java location
+                                                                           // of the Java file from the src
+        System.out.println("Hi " + name + ", Thanks for taking the course");
+        String dateOfBirth = System.console().readLine("What year were you born? ");
+        int age = currentYear - Integer.parseInt(dateOfBirth);
+        return "So you are " + age + " years old.";
+    }
+
+    public static String getInputFromScanner(int currentYear) {
+        return "";
+    }
+}
