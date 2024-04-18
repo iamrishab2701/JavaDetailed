824be77103a8d783068d759cdd5b9749bb0ec829
iamrishab2701 <singhrishab166@gmail.com>
2024-04-18 23:52:23 +0530

    OOPS start

---
+++ src/basics2/Car.java
@@ -0,0 +1,18 @@
+package basics2;
+
+public class Car {
+
+    private String make;
+    private String model;
+    private String color;
+    private int doors;
+    private boolean convertible;
+
+    public void describeCar() {
+        System.out.println(doors + "-Door " +
+                color + " " +
+                make + " " +
+                model + " " +
+                (convertible ? "Convertible" : ""));
+    }
+}
