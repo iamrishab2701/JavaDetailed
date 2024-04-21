329d76f250ca2499c1b5cf3c4baa57f57fe125e3
iamrishab2701 <singhrishab166@gmail.com>
2024-04-21 13:39:01 +0530

    Excise

---
+++ src/CodingExcercise/CarpetCostCalculator/Calculator.java
@@ -0,0 +1,16 @@
+package CodingExcercise.CarpetCostCalculator;
+
+public class Calculator {
+
+    private Floor floor;
+    private Carpet carpet;
+
+    public Calculator(Floor floor, Carpet carpet){
+        this.floor = floor;
+        this.carpet = carpet;
+    }
+
+    public void getTotalCost(){
+
+    }
+}
